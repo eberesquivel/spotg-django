d512a3b355578a173ebc0e5b4d362c761cfd1106
eberesquivel <eberesquivel@gmail.com>
2020-04-23 22:23:00 -0500

    cambios

--- augment_calc/db/sql/augment_type.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `augment_type` (`id`, `type`) VALUES ('1', 'active');
-INSERT INTO `augment_type` (`id`, `type`) VALUES ('2', 'chance');
-INSERT INTO `augment_type` (`id`, `type`) VALUES ('3', 'passive');
